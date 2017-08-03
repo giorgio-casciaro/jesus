035d3ed4646a53359c6fb439762e55c5fd7c988c
giorgio-casciaro <giorgio.casciaro@gmail.com>
2017-08-03 09:30:51 +0200

    removed chalk dep

--- tests/microTest.js
+++ tests/microTest.js
@@ -1,5 +1,4 @@
 var assert = require('assert')
-const chalk = require('chalk')
 
 module.exports = function getTest (name) {
   console.info(`## TEST ${name} ##`)
@@ -8,10 +7,10 @@ module.exports = function getTest (name) {
     testNumber++
     try {
       assert.deepStrictEqual(actual, expected, 'View create 1 item')
-      console.info(chalk.green(`- ${testNumber} SUCCESS ${message}`))
+      console.info(`- ${testNumber} SUCCESS ${message}`)
       console.log({ test: testNumber, success: true, message: message})
     } catch (error) {
-      console.info(chalk.red(`x ${testNumber} ERROR ${message}`))
+      console.info(`x ${testNumber} ERROR ${message}`)
       console.error({test: testNumber, success: false, message: error.message, actual: error.actual, expected: error.expected})
     }
   }
